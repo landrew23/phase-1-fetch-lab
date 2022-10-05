be70d902e362af9279d6a2db9513a2171b859c33
William Killett <williamkillettiii@yahoo.com>
2022-10-04 18:15:04 -0700

    commit tree 3a98c9649cba5aa48f592c82f0fa90dd40ec9153

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ function fetchBooks() {
+return  fetch('https://anapioficeandfire.com/api/books')
+ .then(response => response.json())
+ .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -13,5 +14,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+ fetchBooks();
 });
